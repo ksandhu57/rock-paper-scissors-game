bbe043714ba5dde5b0a18da72d2bf7d226bd6552
Kevin Sandhu <115171986+ksandhu57@users.noreply.github.com>
2022-11-12 14:16:53 -0500

    added playRound function and playerCHoice/computerChoice variables

--- javascript.js
+++ javascript.js
@@ -1,15 +1,30 @@
 function getComputerChoice() {
-    const result = Math.floor(Math.random() * 100);
+    let result = Math.floor(Math.random() * 100);
        if (result <= 33) {
         return 'rock';
     } else if (result > 33 && result < 67) {
         return 'paper';
     } else {
-        return 'scissor';
+        return 'scissors';
     }
 };
 
-console.log(getComputerChoice());
+let playerChoice = prompt('What is your choice? Rock, Paper, or Scissors?').toLowerCase();
+let computerChoice = getComputerChoice();
 
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'It\'s a tie!';
+    } else if ((playerChoice == 'rock' && computerChoice == 'scissor') || (playerChoice == 'paper' && computerChoice == 'rock') || (playerChoice == 'scissor' && computerChoice == 'paper')) {
+        return `You win! ${playerChoice} beats ${computerChoice}!`; 
+    } else {
+        return `You Lose! ${computerChoice} beats ${playerChoice}!`;
+    }
+}
+
+console.log('COMPUTER PICKS: ' + computerChoice);
+console.log('YOU CHOSE: ' + playerChoice);
+console.log(playRound(playerChoice, computerChoice));
 
 
